fe31589553ceefa1ca5f483d736c84640fcefd1e
Sruti Bhattacharjee <SrutiBh@users.noreply.github.com>
2017-11-20 21:06:12 +0530

    Add files via upload

---
+++ basic_math.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 20:54:57 2017
+
+@author: sruti
+"""
+
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+##################################################
+
+#This program subtracts two numbers
+
+sub = float(num1) - float(num2)
+#Display the sub
+print('The subtraction of {0} and {1} is {2}'.format(num1, num2, sub))
+
+#######################################################
+
+#This program multiplies two numbers
+
+multi = float(num1) * float(num2)
+#Display the multi
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, multi))
+
+############################################################
+
+#This program divides two numbers
+
+div = float(num1) / float(num2)
+# Display the div
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, div))
